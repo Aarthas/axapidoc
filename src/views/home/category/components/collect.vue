d22a611f8c13483837fd4551d29fc8b3ad4323a8
赵黎明 <simple@imac.local>
2017-06-30 10:13:01 +0800

    分类页

---
+++ src/views/home/category/components/collect.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="display: flex;flex-direction: column;">
+        <img style="width: 80px;height: 80px;" :src="items.appImgSrc"/>
+        <div >{{items.appCategoryName}}</div>
+    </div>
+</template>
+
+<script>
+    import Lib from 'assets/js/Lib';
+    export default {
+        components: {},
+        props:{
+            items:Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
