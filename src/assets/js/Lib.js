944d2e0059aa4b282c5412f2f8840e1585c02f15
shenyibin <497721363@qq.com>
2017-07-07 14:07:24 +0800

    init

--- src/assets/js/Lib.js
+++ src/assets/js/Lib.js
@@ -1,11 +1,11 @@
 require('assets/css/common.css');
 
 //注册时，vux必须放在 import Vue from 'vue'; 之前，否侧打包的体积非常大，估计是vux OR vue 抽风了
-import {AlertPlugin, LoadingPlugin} from 'vux'
+import {AlertPlugin } from 'vux'
+import  {LoadingPlugin} from 'vux'
 import  {ConfirmPlugin} from 'vux'
 import  {ToastPlugin} from 'vux'
 
-import {cookie} from 'vux'
 
 
 import Vue from 'vue';
@@ -13,10 +13,9 @@ import Vue from 'vue';
 //------ VUX UI 注册，如果不需要  VUX UI 请删除以下注册 -------
 Vue.use(AlertPlugin); //全局注册alert事件，注册之后，不需要每个页面都import alert
 Vue.use(LoadingPlugin); //全局注册alert事件，注册之后，不需要每个页面都import alert
-//--- VUX UI 注册 END --
 Vue.use(ConfirmPlugin)
 Vue.use(ToastPlugin)
-
+//--- VUX UI 注册 END --
 let Hub = new Vue(); //创建事件中心
 
 
@@ -53,7 +52,6 @@ export default{
     uiutil,
     axios: M,
     constant,
-    cookie,
     Hub,
     vux: avux,
     go
--- src/views/home/category/category.vue
+++ src/views/home/category/category.vue
@@ -1,37 +1,37 @@
 <template>
     <div>
-    <search style="position:fixed; top:0; left: 0;" placeholder="搜索三江购物商品"></search>
-    <div  style="display: flex;flex-direction: row;">
+        <search style="position:fixed; top:0; left: 0;" placeholder="搜索三江购物商品"></search>
+        <div style="display: flex;flex-direction: row;margin-top: 50px;">
 
-        <div style="width: 25%;position:fixed; top:0; left: 0;margin-top: 48px;">
-            <ul>
-                <!--<li v-for="item in list" style=" margin-top: 2px;background-color: #f4f4f4;"  >-->
+            <div style="width: 25%;position:fixed; top:0; left: 0;">
+                <ul>
+                    <!--<li v-for="item in list" style=" margin-top: 2px;background-color: #f4f4f4;"  >-->
                     <!--<div  @click="clickLeft(item.id)" style="text-align: center;color: #999999;height:44px;line-height: 44px;font-size: 14px;">{{item.appCategoryName}}</div>-->
-                <!--</li>-->
-                <li v-for="(item,$index) in list" @click="selectStyle (item, $index);clickLeft(item.id) " :class="{'active':item.active,'unactive':!item.active}" >{{item.appCategoryName}}</li>
-            </ul>
+                    <!--</li>-->
+                    <li v-for="(item,$index) in list" @click="selectStyle (item, $index);clickLeft(item.id) "
+                        :class="{'active':item.active,'unactive':!item.active}">{{item.appCategoryName}}
+                    </li>
+                </ul>
+
+            </div>
+            <div style="width: 75%;background-color: white;margin-left: 25%;margin-bottom: 49px;">
+
+                <ul>
+                    <li v-for="item in categoryModelList">
+                        <collect :item="item"></collect>
+                    </li>
+                </ul>
+
+            </div>
 
         </div>
-        <div style="width: 75%;background-color: white;margin-left: 25%;margin-top: 50px;margin-bottom: 49px;">
-
-            <ul>
-                <li v-for="item in categoryModelList">
-                    <collect :item="item" ></collect>
-                </li>
-            </ul>
-
-        </div>
-
-    </div>
     </div>
 </template>
 
 <script>
     import Lib from 'assets/js/Lib';
-
-    import collect from './components/collection_cell.vue';
-
     import search from '../../../components/search.vue' ;
+    import collect from './components/collection_cell.vue';
 
     var page;
     export default {
@@ -42,44 +42,43 @@
         data(){
             return {
                 active: false,
-                list:[],
-                categoryModelList:[],
+                list: [],
+                categoryModelList: [],
 
             }
         },
         created() {
-          Lib.Hub.$on('change', (categoryId) => { //Hub接收事件
-              Lib.go.go("/views/product/list.html?categoryId="+categoryId);
+            Lib.Hub.$on('change', (categoryId) => { //Hub接收事件
+                Lib.go.go("/views/product/list.html?categoryId=" + categoryId);
             });
         },
 
         mounted(){
             page = this;
             loadLeftData();
-         },
+        },
         methods: {
-            clickLeft:function (itemId) {
+            clickLeft: function (itemId) {
 
                 Lib.axios.axios({
-                    url: 'categorys/'+itemId+'/childs',
+                    url: 'categorys/' + itemId + '/childs',
                     success: function (basebean) {
                         page.categoryModelList = basebean.getData().categoryModelList;
                     }
                 });
             },
             selectStyle (item, index) {
-                page.$nextTick(function () {
-                    page.list.forEach(function (item) {
-                        Vue.set(item,'active',false);
-                    });
-                    Vue.set(item,'active',true);
+
+                page.list.forEach(function (item, index) {
+                    item.active = false;
                 });
+                item.active = true
+
             },
 
         }
     }
-    function loadLeftData(){
-
+    function loadLeftData() {
 
 
         Lib.axios.axios({
@@ -88,9 +87,9 @@
             success: function (basebean) {
 
                 page.list = basebean.getData();
-                let item=basebean.getData()[0];
+                let item = basebean.getData()[0];
                 page.clickLeft(item.id);
-                page.selectStyle (item,1);
+                page.selectStyle(item, 1);
 
             }
         });
@@ -98,23 +97,24 @@
     }
 </script>
 <style scoped>
-    .active{
+    .active {
         text-align: center;
         color: #f03838;
-        height:44px;
+        height: 44px;
         line-height: 44px;
         font-size: 14px;
-        background-color:#ffffff ;
+        background-color: #ffffff;
         margin-top: 2px;
     }
-    .unactive{
+
+    .unactive {
         text-align: center;
         color: #999999;
-        height:44px;
+        height: 44px;
         line-height: 44px;
         font-size: 14px;
         margin-top: 2px;
-        background-color:#f6f6f6 ;
+        background-color: #f6f6f6;
     }
 
 
