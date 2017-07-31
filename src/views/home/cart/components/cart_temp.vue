40eab59527cb7afbebf85ca9b16e20962aefa147
赵黎明 <zhaoliming@sanjiang365.com>
2017-07-31 09:48:02 +0800

    购物车活动信息太长显示不下改用走马灯

--- src/views/home/cart/components/cart_temp.vue
+++ src/views/home/cart/components/cart_temp.vue
@@ -3,7 +3,9 @@
         <!--活动信息-->
         <div v-show="item.typeId!=1" style="display: flex;flex-direction: row;background-color: white;height: 31px;">
             <div style="ine-height: 20px;height:20px;margin-top:5px;margin-left: 8px;text-align:center;background-color:#1aad19;color: white;font-size: 12px;width: 50px;">{{item.name}}</div>
-            <div  style="font-size: 10px;text-align: left;line-height: 30px;margin-left: 4px;color: #333333;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;" >{{item.type}}</div>
+            <!--<div  style="font-size: 10px;text-align: left;line-height: 30px;margin-left: 4px;color: #333333;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;" >{{item.type}}</div>-->
+            <vue-marquee  style="margin-left: 5px;line-height:30px;color: #666666;font-size: 10px;width: 270px;" :content="item.type" class="two"  :showtwo="false"></vue-marquee>
+
         </div>
 
         <!--<div style="height: 1px;background-color: #cbcbcb;"></div>-->
@@ -17,9 +19,10 @@
 
 <script>
 
-    import cart_cell from './cart_cell.vue'
+    import cart_cell from './cart_cell.vue';
+    import marquee from './marquee.vue';
     export default {
-        components: {cart_cell},
+        components: {cart_cell, "vue-marquee": marquee,},
 
         props:{
             item:Object
