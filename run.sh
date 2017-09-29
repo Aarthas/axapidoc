29be8cf5decdff77eee0bdd1e6c9e7dca19db9f2
shenyibin <497721363@qq.com>
2017-09-29 16:47:00 +0800

    remove master

--- run.sh
+++
@@ -1,9 +0,0 @@
-rm -rf dist/
-npm run build
-echo rm  remote dist
-rm -rf /Users/zhangyn/demo/vue/H5Product/wx/sjmarket/*
-echo cp remote dist
-cp -ri dist/* /Users/zhangyn/demo/vue/H5Product/wx/sjmarket
-echo start server
-cd ./dist
-## http-server -p 9090
--- src/views/coupon/usecode/usecode.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-
-import App from './usecode.vue'
-
-new Vue({
-    render: h => h(App)
-}).$mount('#app')
